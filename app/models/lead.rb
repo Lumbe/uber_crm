e23b719e7e5ea3a548e97127807fce5c37490c75
lumbe <just.me.sober@gmail.com>
2016-07-21 07:34:32 +0000

    add :repeated status to Lead

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -3,6 +3,6 @@ class Lead < ActiveRecord::Base
     validates :name, presence: true
     validates :phone, presence: true
     
-    enum status: [:newly, :closed, :converted, :sended]
+    enum status: [:newly, :closed, :converted, :sended, :repeated]
     
 end
