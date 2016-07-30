8154b2f8e545f2c66ebb24e035018e342ecff361
lumbe <just.me.sober@gmail.com>
2016-07-30 14:40:27 +0000

    Devise authentication config

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root 'home#index'
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   resources :leads
-
   resources :articles
   # You can have the root of your site routed with "root"
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,33 +11,55 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_160_718_150_009) do
+ActiveRecord::Schema.define(version: 20160729135948) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'articles', force: :cascade do |t|
-    t.string   'title'
-    t.text     'text'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'leads', force: :cascade do |t|
-    t.string   'name'
-    t.string   'phone'
-    t.string   'email'
-    t.string   'location'
-    t.string   'project'
-    t.string   'square'
-    t.string   'floor'
-    t.text     'question'
-    t.datetime 'created_at',                     null: false
-    t.datetime 'updated_at',                     null: false
-    t.string   'region'
-    t.string   'source'
-    t.boolean  'online_request', default: false
-    t.boolean  'come_in_office', default: false
-    t.boolean  'phone_call',     default: false
-    t.integer  'status',         default: 0
+  create_table "leads", force: :cascade do |t|
+    t.string   "name"
+    t.string   "phone"
+    t.string   "email"
+    t.string   "location"
+    t.string   "project"
+    t.string   "square"
+    t.string   "floor"
+    t.text     "question"
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.string   "region"
+    t.string   "source"
+    t.boolean  "online_request", default: false
+    t.boolean  "come_in_office", default: false
+    t.boolean  "phone_call",     default: false
+    t.integer  "status",         default: 0
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
 end
