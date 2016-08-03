d3dd238a0aa65fa20a805134cb730fab08e5e8be
lumbe <just.me.sober@gmail.com>
2016-08-03 15:25:31 +0000

    add :admin to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   has_many :competitors
   has_many :comments, as: :commentable      # As owner who created a comment.
 
-  enum role: [:visitor, :admin, :chief, :marketer, :senior_manager, :manager, :tech_designer, :estimator, :accountant]
+  enum role: [:visitor, :chief, :marketer, :senior_manager, :manager, :tech_designer, :estimator, :accountant]
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
