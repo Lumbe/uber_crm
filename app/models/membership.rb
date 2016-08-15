d917f4dc75e64903be8f81f805bb668fcd1a04a1
lumbe <just.me.sober@gmail.com>
2016-08-15 11:56:30 +0000

    move role attribute from user to membership

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,4 +1,6 @@
 class Membership < ActiveRecord::Base
   belongs_to :department
   belongs_to :user
+  
+  enum role: [:visitor, :chief, :marketer, :senior_manager, :manager, :tech_designer, :estimator, :accountant]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160815092218) do
+ActiveRecord::Schema.define(version: 20160815113846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -156,8 +156,9 @@ ActiveRecord::Schema.define(version: 20160815092218) do
   create_table "memberships", force: :cascade do |t|
     t.integer  "department_id"
     t.integer  "user_id"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",                null: false
+    t.datetime "updated_at",                null: false
+    t.integer  "role",          default: 0
   end
 
   add_index "memberships", ["department_id"], name: "index_memberships_on_department_id", using: :btree
@@ -178,7 +179,6 @@ ActiveRecord::Schema.define(version: 20160815092218) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.integer  "role",                   default: 0
     t.boolean  "admin",                  default: false
   end
 
