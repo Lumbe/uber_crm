8d852faa18d22a490326d7614ae396ace4a4b05c
lumbe <just.me.sober@gmail.com>
2016-08-27 12:28:06 +0000

    admin: delete user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -27,6 +27,10 @@ class Admin::UsersController < ApplicationController
   end
 
   def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to admin_users_path
   end
   
   def become
