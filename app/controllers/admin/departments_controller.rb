973680f75e847d7991c046d21fff76ab62cde285
lumbe <just.me.sober@gmail.com>
2016-08-30 09:02:01 +0000

    departments: list, create, delete

--- app/controllers/admin/departments_controller.rb
+++ app/controllers/admin/departments_controller.rb
@@ -1,22 +1,41 @@
 class Admin::DepartmentsController < ApplicationController
   def index
+    @departments = Department.all.order(created_at: :asc)
   end
 
   def show
   end
 
   def new
+    @department = Department.new
+  end
+  
+  def create
+    @department = Department.new(department_params)
+
+    if @department.save
+      redirect_to admin_departments_path
+    else
+      render 'new'
+    end
   end
 
   def edit
-  end
-
-  def create
   end
 
   def update
   end
 
   def destroy
+    @department = Department.find(params[:id])
+    @department.destroy
+
+    redirect_to admin_departments_path
+  end
+  
+  private
+  
+  def department_params
+    params.require(:department).permit(:name)
   end
 end
