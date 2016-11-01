37b62ec2587c69061f4fac00f38b98d6ec6d12a3
lumbe <just.me.sober@gmail.com>
2016-11-01 22:09:10 +0000

    add status :sended, do_not_call & alt_email to contact

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161027125107) do
+ActiveRecord::Schema.define(version: 20161101211438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
-    t.string   "commentable_type"
     t.integer  "commentable_id"
+    t.string   "commentable_type"
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
     t.text     "body"
@@ -64,11 +71,13 @@ ActiveRecord::Schema.define(version: 20161027125107) do
     t.boolean  "online_request"
     t.boolean  "come_in_office"
     t.boolean  "phone_call"
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
     t.integer  "department_id"
     t.integer  "status",         default: 0
     t.datetime "proposal_sent"
+    t.string   "alt_email"
+    t.boolean  "do_not_call",    default: false
   end
 
   create_table "customers", force: :cascade do |t|
