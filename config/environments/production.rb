af2f698c7e0f264e86b919f0d0dc56d0211269b2
lumbe <just.me.sober@gmail.com>
2016-11-01 12:27:36 +0000

    init secret_base_key in  environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # Do not lookup secret_key_base in config/secrets.yml, instead of load it from env variable
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
 end
