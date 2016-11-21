c31b72eb3a7de8130b9860e54990b6fc5e5b1b05
lumbe <just.me.sober@gmail.com>
2016-11-21 20:30:11 +0000

    drop not needed table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161113211315) do
+ActiveRecord::Schema.define(version: 20161121202716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,8 +48,8 @@ ActiveRecord::Schema.define(version: 20161113211315) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id"
-    t.string   "commentable_type"
     t.integer  "commentable_id"
+    t.string   "commentable_type"
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
     t.text     "body"
