a55fecd161f8640cc0fda3d4716da6e0906ea14f
lumbe <just.me.sober@gmail.com>
2016-11-22 10:25:28 +0000

    yandex smtp production config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,6 +60,7 @@ Rails.application.configure do
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
 config.action_mailer.delivery_method = :smtp
+config.action_mailer.raise_delivery_errors = true
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
