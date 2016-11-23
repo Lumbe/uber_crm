a6f4b7bff3318d92b6605cddae611cfd94caeebe
lumbe <just.me.sober@gmail.com>
2016-11-23 20:05:11 +0000

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,16 +86,15 @@ Rails.application.configure do
   
   config.assets.digest = true
 
-  config.action_mailer.default_url_options = {:host => '95.85.55.222'}
+  config.action_mailer.default_url_options = {:host => "localhost"}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
+    address:              "smtp.gmail.com",
     port:                 587,
-    domain:               'gmail.com',
+    domain:               "gmail.com",
     user_name:            ENV["EMAIL_SENDER_ADDRESS"],
     password:             ENV["EMAIL_SENDER_PASSWORD"],
-    authentication:       'login',
-    enable_starttls_auto: true,
-    tls: false
+    authentication:       "plain",
+    enable_starttls_auto: true
   }
 end
