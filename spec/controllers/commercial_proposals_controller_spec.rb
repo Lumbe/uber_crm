2052968a80589082ff3ee40a245afd3a85581473
lumbe <just.me.sober@gmail.com>
2016-12-05 15:32:02 +0000

    specs for commercial proposal model & controller

--- spec/controllers/commercial_proposals_controller_spec.rb
+++ spec/controllers/commercial_proposals_controller_spec.rb
@@ -1,25 +1,75 @@
 require 'rails_helper'
 
 RSpec.describe CommercialProposalsController, type: :controller do
+  login_user('manager')
+  before :each do
+    @user = subject.current_user
+    @contact = create(:contact, department_id: @user.current_department_id)
+  end
 
   describe "GET #new" do
     it "returns http success" do
-      get :new
+      get :new, params: { contact_id: @contact.id }
       expect(response).to have_http_status(:success)
+    end
+
+    it "renders 'new' template" do
+      get :new, params: { contact_id: @contact.id }
+      expect(response).to render_template(:new)
+    end
+
+    it "assigns a new commercial_proposal to @commercial_proposal" do
+      get :new, params: { contact_id: @contact.id }
+      expect(assigns(:commercial_proposal)).to be_a_new(CommercialProposal)
     end
   end
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  describe "POST #create" do
+
+    context "with valid attributes" do
+      it "saves new commercial_proposal to database" do
+        post :create, params: { contact_id: @contact.id, user_id: @user.id, commercial_proposal: attributes_for(:commercial_proposal) }
+        expect(CommercialProposal.count).to eq(1)
+      end
+
+      it "redirects to created commercial proposal" do
+        post :create, params: { contact_id: @contact.id, user_id: @user.id, commercial_proposal: attributes_for(:commercial_proposal) }
+        expect(response).to redirect_to contact_commercial_proposal_path(assigns(:contact), assigns(:commercial_proposal))
+      end
+
     end
+
+    context "with invalid attributes" do
+      it "don't save invalid commercial_proposal to database" do
+        post :create, params: { contact_id: @contact.id, user_id: @user.id, commercial_proposal: attributes_for(:invalid_commercial_proposal) }
+        expect(CommercialProposal.count).to eq(0)
+      end
+
+      it "renders new template" do
+        post :create, params: { contact_id: @contact.id, user_id: @user.id, commercial_proposal: attributes_for(:invalid_commercial_proposal) }
+        expect(response).to render_template(:new)
+      end
+    end
+
   end
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      commercial_proposal = create(:commercial_proposal)
+      get :show, params: { id: commercial_proposal.id, contact_id: @contact.id }
       expect(response).to have_http_status(:success)
+    end
+
+    it "assigns the requested commercial_proposal to @commercial_proposal" do
+      commercial_proposal = create(:commercial_proposal)
+      get :show, params: { id: commercial_proposal.id, contact_id: @contact.id }
+      expect(assigns(:commercial_proposal)).to eq(commercial_proposal)
+    end
+
+    it "renders :show" do
+      commercial_proposal = create(:commercial_proposal)
+      get :show, params: { id: commercial_proposal.id, contact_id: @contact.id }
+      expect(response).to render_template('show')
     end
   end
 
--- spec/factories/commercial_proposals.rb
+++ spec/factories/commercial_proposals.rb
@@ -1,11 +1,20 @@
 FactoryGirl.define do
-  factory :commercial_proposal do
-    name                        { Faker::Name.name }
+  factory :commercial_proposal, class: CommercialProposal do
+    project_name                { Faker::Name.name }
     house_kit_price             { Faker::Number.between(4000, 15000) }
     additional_services_price   { Faker::Number.between(5000, 16000) }
     association :contact, factory: :contact
     association :user, factory: :user
     discount                    { Faker::Number.between(5, 20) }
     dollar_exchange_rate        { Faker::Number.between(20, 30) }
+  end
+  factory :invalid_commercial_proposal, class: CommercialProposal do
+    project_name                { nil }
+    house_kit_price             { nil }
+    additional_services_price   { nil }
+    association :contact, factory: :contact
+    association :user, factory: :user
+    discount                    { Faker::Number.between(5, 20) }
+    dollar_exchange_rate        { nil }
   end
 end
