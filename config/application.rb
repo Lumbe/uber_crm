2f1c344e355395298a9df14e69876ec07855a206
lumbe <just.me.sober@gmail.com>
2016-12-09 09:30:20 +0000

    config: autoload files in lib directory

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Workspace
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :ru
+    config.autoload_paths += %W(#{config.root}/lib)
 
     # default sass syntax fr rails generators
     if Rails.configuration.respond_to?(:sass)
