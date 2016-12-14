325e2b52694470ea5420ce6bfe69e80e1be173ac
lumbe <just.me.sober@gmail.com>
2016-12-14 14:35:10 +0000

    view for commercial proposal (send by email button, list of sended cps) & CommercialProposals#send_by_email action

--- app/controllers/commercial_proposals_controller.rb
+++ app/controllers/commercial_proposals_controller.rb
@@ -18,6 +18,15 @@ class CommercialProposalsController < ApplicationController
     @commercial_proposal = CommercialProposal.find(params[:id])
   end
 
+  def send_by_email
+    @commercial_proposal = CommercialProposal.find(params[:commercial_proposal_id])
+    @contact = Contact.find(params[:contact_id])
+    @user = current_user
+    CommercialProposalMailer.send_commercial_proposal(@contact, @user, @commercial_proposal).deliver_now
+    flash[:notice] = "Коммерческое предложение для #{@commercial_proposal.project_name} успешно отправлено на почту: #{@contact.email}"
+    redirect_back fallback_location: @commercial_proposal
+  end
+
   private
 
   def commercial_proposal_params
