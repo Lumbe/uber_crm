8d5dde615777a252719e3c4825ba365ef2021a67
lumbe <just.me.sober@gmail.com>
2016-12-20 11:37:10 +0000

    set mailgun domail to 'mg.servus.vn.ua'

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -78,6 +78,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.mailgun_settings = {
           api_key: 'key-a59abbc67d0dca5f43905227e9cf42ba',
-          domain: 'sandbox6c18ff9554cc49aeab97c7fad99c0440.mailgun.org'
+          domain: 'mg.servus.vn.ua'
+          # domain: 'sandbox6c18ff9554cc49aeab97c7fad99c0440.mailgun.org'
   }
 end
