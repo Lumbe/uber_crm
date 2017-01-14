1dff0b21f3aba98a9a02e4fb482b97fdd42e1343
lumbe <just.me.sober@gmail.com>
2017-01-14 14:21:49 +0000

    mail observer: include lead mailer

--- lib/observers/mail_observer.rb
+++ lib/observers/mail_observer.rb
@@ -2,15 +2,15 @@ module Observers
   class MailObserver
     def self.delivered_email(message)
       return unless message.respond_to?(:observer_args)
-      if message.observer_args[:commercial_proposal].present?
+      if message.observer_args[:commercial_proposal].present? || message.observer_args[:lead].present?
         sent_email = Message.new(
            from: message.from.join(','),
            to: message.to.join(','),
            subject: message.subject,
            message_id: message.message_id,
-           user_id: message.observer_args[:user].id,
-           commercial_proposal_id: message.observer_args[:commercial_proposal].id
+           user_id: message.observer_args[:user].id
         )
+        sent_email.commercial_proposal_id = message.observer_args[:commercial_proposal].id if message.observer_args[:commercial_proposal].present?
 
         sent_email.body = if message.html_part.present? || message.text_part.present?
                              message.html_part.blank? ? message.text_part.body.raw_source : message.html_part.body.raw_source
