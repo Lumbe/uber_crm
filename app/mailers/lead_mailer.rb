523605b455a726903f60a0e48622bf50b211abad
lumbe <just.me.sober@gmail.com>
2017-01-14 14:53:22 +0000

    Lead mailer refactoring

--- app/mailers/lead_mailer.rb
+++ app/mailers/lead_mailer.rb
@@ -1,8 +1,26 @@
 class LeadMailer < ApplicationMailer
-  default from: "Mailgun Test <postmaster@sandbox6c18ff9554cc49aeab97c7fad99c0440.mailgun.org>"
+  default from: "Сервус <office@servus.km.ua>"
+  add_template_helper ApplicationHelper
+
+  def mail(headers = {}, &block)
+    message = super(headers.except(:observer_args, :interceptor_args), &block)
+    if headers[:observer_args].present?
+      message.class_eval { attr_accessor :observer_args }
+      message.observer_args = headers[:observer_args]
+    end
+    if headers[:interceptor_args].present?
+      message.class_eval { attr_accessor :interceptor_args }
+      message.interceptor_args = headers[:interceptor_args]
+    end
+    message
+  end
 
   def send_lead(recipient_email, sender, lead)
     @lead = lead
-    mail(to: recipient_email, subject: "Новый лид")
+    @user = sender
+    mail from: @lead.department.email,
+        to: recipient_email,
+        subject: "Новый лид",
+        observer_args: {user: @user, lead: @lead}
   end
 end
