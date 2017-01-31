f6c62c8144e0a417cdaa50a07d4be244dbd316fa
lumbe <just.me.sober@gmail.com>
2017-01-31 08:30:47 +0000

    factory and model spec for Message

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,5 +1,12 @@
 FactoryGirl.define do
   factory :message do
-    
+    from      { Faker::Internet.email }
+    to      { Faker::Internet.email }
+    user
+  end
+  factory :invalid_message, class: 'Message' do
+    from      { nil }
+    to        { nil }
+    user      { nil }
   end
 end
